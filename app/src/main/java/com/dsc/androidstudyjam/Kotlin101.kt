3e5811db096ff597034809236d503f59fabc9e9f
lucasnobile <12Archa12!>
2020-11-15 12:56:47 -0300

    Add Hello World, Immutability, Null Safety, and Collections.

--- app/src/main/java/com/dsc/androidstudyjam/Kotlin101.kt
+++ app/src/main/java/com/dsc/androidstudyjam/Kotlin101.kt
@@ -2,4 +2,63 @@ package com.dsc.androidstudyjam
 
 fun main() {
     println("Hello World")
+
+    println("Immutability: var vs val")
+    var name: String = "Android"
+    name += " Study Jam"
+    val version = "101"
+//    version = "2" // compilation error
+    println("String template $name $version")
+
+    println("Null Safety")
+    var a: String = "a"
+//    a = null // compilation error
+    println(a)
+
+    var b: String? = "b"
+    b = null
+    println(b)
+
+    // Safe call operator
+    println(b?.length)
+
+    // Elvis operator
+    val c = b?.length ?: -1
+    println(c)
+
+    println("Collections")
+    // Immutable list
+    val list = listOf(1, 2, 3)
+//    val list = listOf<Int>(1, 2, "3") // compilation error
+//    list.add(4) // compilation error
+    println(list)
+
+    val first = list[0] // or list.first()
+    println(first)
+
+    // Mutable list
+    val list2 = mutableListOf(1, 2, 3)
+    list2.add(4)
+    println(list2)
+
+    // Iteration
+    list2.forEach { println(it) }
+
+    val pares = list2.filter { num ->
+        num % 2 == 0
+    }
+    println("Pares $pares")
+
+    // Immutable map
+    mapOf(
+        1 to "1",
+        2 to "2"
+    )
+
+    // Mutable map
+    val map = mutableMapOf(
+        "1" to 1,
+        "2" to 2
+    )
+    map["3"] = 2 // or map.put("3", 2)
 }
